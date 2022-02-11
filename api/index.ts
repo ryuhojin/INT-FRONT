7c1d0cb61abf5461354cce5e5b8aed7420c8eb56
ryuhojin <interarea@naver.com>
2022-02-11 12:16:02 +0900

    test

--- api/index.ts
+++ api/index.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const service = axios.create({
     timeout: 3000,
-    baseURL: "http://3.17.152.174:8080/api/",
+    baseURL: "https://3.17.152.174:8080/api/",
 })
 
 service.interceptors.request.use((config) => {
