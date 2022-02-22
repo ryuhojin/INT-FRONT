73f022adb2ad5881279ca152624002e584a6e976
ryuhojin <interarea@naver.com>
2022-02-22 09:35:59 +0900

    http to https

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 const service = axios.create({
     timeout: 3000,
-    baseURL: "http://3.20.158.73:8080/api/",
+    baseURL: "https://3.20.158.73:8080/api/",
 })
 
 service.interceptors.request.use((config) => {
