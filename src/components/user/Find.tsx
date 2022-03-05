c01e8ea0f163fb8355e832ea823dd5126a0516a7
류호진 <interarea@naver.com>
2022-03-05 15:33:45 +0900

    update : find password disabled userId

--- src/components/user/Find.tsx
+++ src/components/user/Find.tsx
@@ -59,7 +59,7 @@ const Find = ({
     }
     return <StyledFind>
         <div>
-            <input value={userId} onChange={setUserId} type="text" placeholder='고객님의 아이디를 입력해주세요.' />
+            <input value={userId} onChange={setUserId} disabled type="text" placeholder='고객님의 아이디를 입력해주세요.' />
             <input value={password} onChange={setPassword} type="password" placeholder='변경할 비밀번호를 입력해주세요.' />
             <input value={passwordChk} onChange={setPasswordChk} type="password" placeholder='변경할 비밀번호를 입력해주세요.(확인)' />
             <input value={authNum} onChange={setAuthNum} type="text" placeholder='인증번호를 입력해주세요.' />
