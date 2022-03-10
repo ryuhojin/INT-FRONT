51e85683c1f940a92cc4679a1008f71a6353f247
류호진 <interarea@naver.com>
2022-03-10 09:06:02 +0900

    update https:

--- api/index.ts
+++ api/index.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const service = axios.create({
     timeout: 3000,
-    baseURL: "http://3.20.158.73:8080/api/",
+    baseURL: "https://notworking-api.link/api/",
 })
 
 service.interceptors.request.use((config: any) => {
